beded713569756431a58aa4b968540fc780fc0f3
Matt0634 <56381355+Matt0634@users.noreply.github.com>
2020-01-28 23:00:35 -0600

    Update FirstTestCase.java

--- src/FirstTestCase.java
+++ src/FirstTestCase.java
@@ -1,12 +1,5 @@
 public class FirstTestCase {
     public static void main(String[] args) {
-        System.out.println("Welcome to Cybertek!");
-        System.out.println("Added this lane!");
-        System.out.println("Cybertek is orginizing some nice events!");
-        System.out.println("Added this lane!");
-        System.out.println("Cybertek is orginizing some nice events!");
-        System.out.println("Added this lane!");
-        System.out.println("Cybertek is orginizing some nice events!");
 
         System.out.println("Cybertek is the best it course for testers' candidate!");
         System.out.println("You should join the 'Intro Session' of Cybertek!");
