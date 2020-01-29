a984c69d7efefadbfce9aaa3c0594d11376cd6e0
metoceto <57574169+metoceto@users.noreply.github.com>
2020-01-28 22:53:54 -0600

    third  push!

--- src/FirstTestCase.java
+++ src/FirstTestCase.java
@@ -7,5 +7,8 @@ public class FirstTestCase {
         System.out.println("Cybertek is orginizing some nice events!");
         System.out.println("Added this lane!");
         System.out.println("Cybertek is orginizing some nice events!");
+
+        System.out.println("Cybertek is the best it course for testers' candidate!");
+        System.out.println("You should join the 'Intro Session' of Cybertek!");
     }
 }
