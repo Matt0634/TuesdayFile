465eba681bf1f2ef953040ed8790e76e569bf7e1
Matt0634 <56381355+Matt0634@users.noreply.github.com>
2020-01-28 23:19:41 -0600

    Update FirstTestCase.java

--- src/FirstTestCase.java
+++ src/FirstTestCase.java
@@ -2,6 +2,9 @@ public class FirstTestCase {
     public static void main(String[] args) {
 
         System.out.println("Cybertek is the best it course for testers' candidate!");
-        System.out.println("You should join the 'Intro Session' of Cybertek!");
+       
+        
+        
+            
     }
 }
